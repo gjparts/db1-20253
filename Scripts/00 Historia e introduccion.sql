0ae29e1af13e34e29ee30699c01ef8aca4d1eb18
GJParts <39961515+gjparts@users.noreply.github.com>
2025-10-08 09:06:00 -0600

    commit 0810202530801

---
+++ Scripts/00 Historia e introduccion.sql
@@ -0,0 +1,55 @@
+--comentario de una linea
+
+/*comentario
+de varias
+lineas*/
+
+/*UN POCO DE HISTORIA
+SQL SERVER: Es el servidor de base de datos producido por Microsoft
+basandose en el estandar SQL.
+
+* La version 1.0 nace en 1989 en conjunto con IBM para su sistema operativo OS/2
+  Las versiones 2 y 3 fueron destinadas a IBM
+
+* La version 4.0 viene en 1993 cuando Microsoft crea el sistema operativo
+  Windows NT (New Technology)
+
+* Para 1999 se lanza la version 7.0, la version mas popular de SQL SERVER
+
+* Para 2005 comienzan a nombrar las versiones con años (internamente siempre llevan
+  un numero de version). Luego se lanzan las versiones 2008, 2012, 2014, 2016, 2017, 2019, 2022
+
+* La version 2022 es la ultima liberada hasta 2025
+
+TIPOS DE INSTRUCCIONES EN SQL SERVER
+Se agrupan en 4 categorias o grupos:
+1)	DDL (Data Definition Language)
+	Son las instrucciones que se utilizan para gestionar objetos de base de datos
+	Las mas conocidas son:
+	* CREATE
+	* DROP
+	* ALTER
+	* TRUNCATE
+
+2)	DML (Data Manipulation Language)
+	Se utilizan para interactuar con la informacion almacenada en la base de datos
+	Las mas conocidas son:
+	* SELECT
+	* INSERT
+	* UPDATE
+	* DELETE
+
+3)	DCL (Data Control Language)
+	Son utilizadas para gestionar la seguridad y los permisos en la base de datos
+	Las mas conocidas son:
+	* GRANT
+	* REVOKE
+	* DENY
+
+4)	TCL (Transaction Control Language)
+	Se usan para administrar las transacciones que son un conjunto de operaciones
+	que se tratan como una sola unidad, estas son:
+	* BEGIN TRANSACTION
+	* COMMIT
+	* ROLLBACK
+*/
