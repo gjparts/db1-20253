5955b61ee8cbdd2273f1634af176dd038a4b33e5
GJParts <39961515+gjparts@users.noreply.github.com>
2025-10-28 09:23:49 -0600

    commit 281020258349

--- Scripts/03 ORDER BY y WHERE.sql
+++ Scripts/03 ORDER BY y WHERE.sql
@@ -83,4 +83,163 @@ SELECT TRIM(Nombre) as Nombre, Pais
 FROM Cliente
 ORDER BY 2 ASC, 1 ASC
 
---uso del WHERE: se usa para filtrar la informacion de una consulta.-----------------------------
+--Clausula WHERE: se usa para filtrar la informacion de una consulta.-----------------------------
+--Producto cuyo codigo sea 
+SELECT ProductoID, Codigo, Descripcion, CostoPromedio, PrecioVenta
+FROM Producto
+WHERE Codigo = 'P0302'
+
+--Productos cuyo codigo sea P0107, P0206 o P0409
+SELECT ProductoID, Codigo, Descripcion, CostoPromedio, PrecioVenta
+FROM Producto
+WHERE Codigo = 'P0107' OR Codigo = 'P0206' OR Codigo = 'P0409'
+
+--Productos cuyo codigo sea P0107, P0206 o P0409 (Usando IN)
+SELECT ProductoID, Codigo, Descripcion, CostoPromedio, PrecioVenta
+FROM Producto
+WHERE Codigo IN ('P0107','P0206','P0409')
+--IN es mas práctico cuando necesitamos seleccionar cualquier de los valores
+--de una lista para determinado campo
+
+--IN tambien se puede usar con datos numericos
+--Productos cuyo ProductoID sea 4, 8, 11 o 15
+SELECT ProductoID, Codigo, Descripcion, CostoPromedio, PrecioVenta
+FROM Producto
+WHERE ProductoID IN (4,8,11,15)
+--Si IN no encuentra alguno de los valores de la lista simplemente
+--no lo incluye en el resultado
+
+--Existe el operador NOT el cual invierte el resultado de una condicion
+--se puede combinar con el IN
+--Productos cuyo Codigo NO SEA P0101, P0102 o P0110
+SELECT ProductoID, Codigo, Descripcion, CostoPromedio, PrecioVenta
+FROM Producto
+WHERE NOT Codigo IN ('P0101','P0102','P0110')
+
+--Uso del operador DISTINTO DE <>   (NO ES IGUAL A)
+--Facturas cuyo ClienteID sea distinto de 1
+SELECT *
+FROM FacturaCab
+WHERE ClienteID <> 1
+
+--el anterior usando NOT
+--Facturas cuyo ClienteID sea distinto de 1
+SELECT *
+FROM FacturaCab
+WHERE NOT ClienteID = 1
+
+--Uso del operador LIKE (parecido o similar)
+--En SQL SERVER este operador solo se puede utilizar en campos que manejen texto
+--Si la base de datos utiliza un COLLATION que maneje CI (Case Insensitive)
+--entonces LIKE va a ignorar mayusculas y minusculas
+
+--Mostrar los productos que tengan el texto LLEVAR en cualquier parte de su descripcion
+SELECT *
+FROM Producto
+WHERE Descripcion LIKE '%LLEVAR%'
+
+--Productos que llevan la palabra SENCILLA en cualquier parte de la descripcion
+SELECT *
+FROM Producto
+WHERE Descripcion LIKE '%SENCILLA%'
+
+--Productos cuya descripcion comience con la letra A
+SELECT *
+FROM Producto
+WHERE Descripcion LIKE 'A%'
+--aqui estos diciendo que el texto comienza con A y el % indica que no importa
+--que caracteres vayan despues
+--al % se le conoce Wildcard (comodín)
+
+--la consulta anterior se puede hacer sin LIKE de esta forma:
+SELECT *
+FROM Producto
+WHERE LEFT(Descripcion,1) = 'A'
+
+--productos cuya descripcion comience con la palabra pollo
+SELECT *
+FROM Producto
+WHERE Descripcion LIKE 'pollo%'
+
+--productos cuya descripcion termine con la palabra VAR
+SELECT *
+FROM Producto
+WHERE Descripcion LIKE '%VAR'
+
+--Tambien es posible incrustar los wildcard en medio de las expresiones:
+--productos cuya descripcion lleve la palabra MIXTA antes que la palabra LLEVAR
+SELECT *
+FROM Producto
+WHERE Descripcion LIKE '%MIXTA%LLEVAR%'
+
+--productos cuya descripcion lleve la palabra MIXTA antes que la palabra LLEVAR
+--ó que lleve la palabra LLEVAR antes que la palabra MIXTA
+SELECT *
+FROM Producto
+WHERE Descripcion LIKE '%MIXTA%LLEVAR%' OR Descripcion LIKE '%LLEVAR%MIXTA%'
+
+--Otro comodin que se puede usar con LIKE es el GUION BAJO
+--Este se utiliza para indicar que no importa que caracter vaya en determinada posicion:
+--Mostrar los productos cuya tercera letra sea A
+SELECT *
+FROM Producto
+WHERE Descripcion LIKE '__A%'
+
+--tambien se puede hacer con SUBSTRING
+SELECT *
+FROM Producto
+WHERE SUBSTRING(Descripcion,3,1) = 'A'
+
+--mostrar los productos cuya penultima letra sea E
+SELECT *
+FROM Producto
+WHERE Descripcion LIKE '%E_'
+
+--mostrar los productos cuya penultima letra sea E
+SELECT *
+FROM Producto
+WHERE TRIM(Descripcion) LIKE '%E_'  --TRIM para evitar que espacios en blanco afecten el resultado
+
+--LIKE tambien soporta los wildcard [ ] los cuales se utilizan
+--para establecer listas de seleccion
+
+--Mostrar los productos cuya segunda letra sea A, E, I
+SELECT *
+FROM Producto
+WHERE Descripcion LIKE '_[AEI]%'
+
+--Mostrar los productos cuya primera letra sea S, C, M
+SELECT *
+FROM Producto
+WHERE Descripcion LIKE '[SCM]%'
+
+--Mostrar los productos cuya primer letra este entre A y D
+--ordenarlos por Descripcion
+SELECT *
+FROM Producto
+WHERE Descripcion LIKE '[A-D]%'
+ORDER BY Descripcion
+
+--los anterior se puede hacer con SUBSTRING:
+SELECT *
+FROM Producto
+WHERE SUBSTRING(Descripcion,1,1) IN ('A','B','C','D')
+ORDER BY Descripcion
+
+--Mostrar los prpductos cuya primer letra NO este entre A y D
+--ordenarlos por Descripcion
+SELECT *
+FROM Producto
+WHERE NOT Descripcion LIKE '[A-D]%'
+ORDER BY Descripcion
+
+--Operador MODULO: devuelve el residuo de la division --------------------
+--Productos cuyo ProductoID sea PAR
+SELECT *
+FROM Producto
+WHERE ProductoID%2 = 0
+
+--Productos cuyo ProductoID sea IMPAR
+SELECT *
+FROM Producto
+WHERE ProductoID%2 = 1
