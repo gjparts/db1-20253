03a6f40b31564902caecb4217ea424dc791527fd
GJParts <39961515+gjparts@users.noreply.github.com>
2025-11-10 10:56:08 -0600

    commit 1011202524316

--- Scripts/06 Funciones de agregado, GROUP BY y HAVING.sql
+++ Scripts/06 Funciones de agregado, GROUP BY y HAVING.sql
@@ -226,3 +226,139 @@ FROM FacturaCab
 WHERE Estado = 'NOR'
 --Observe que dentro de SUM colocamos una toma de decision de lo que se va a sumar de acuerdo
 --al cumplimiento de una condicion.
+
+/*Para las facturas cuyo estado sea NOR muestre en una columna la suma del total de las facturas
+realizadas en 2010 y en otra columna el total de las facturas realizadas en 2011 agrupadas por forma de pago*/
+SELECT	Pago,
+		SUM( CASE WHEN YEAR(Fecha) = 2010 THEN Total ELSE 0.00 END ) as [Facturado en 2010],
+		SUM( CASE WHEN YEAR(Fecha) = 2011 THEN Total ELSE 0.00 END ) as [Facturado en 2011]
+FROM FacturaCab
+WHERE Estado = 'NOR'
+GROUP BY Pago
+
+/*Para las facturas realizadas con estado NOR muestre en una columna la cantidad de facturas
+realizadas al Contado y la cantidad realizada al Credito*/
+SELECT	COUNT( CASE WHEN Tipo = 'Contado' THEN 1 ELSE NULL END ) as [Facturas de Contado],
+		COUNT( CASE WHEN Tipo = 'Credito' THEN 1 ELSE NULL END ) as [Facturas de Credito]
+FROM FacturaCab
+WHERE Estado = 'NOR'
+/*Observe que si se cumple la condicion se retorna un valor para ser contado en caso contrario
+se retorna NULL el cual no sera contado por COUNT.*/
+
+/*Para las facturas realizadas con estado NOR muestre en una columna la cantidad de facturas
+realizadas al Contado y la cantidad realizada al Credito agrupadas por forma de Pago ordenadas
+por forma de pago*/
+SELECT	Pago,
+		COUNT( CASE WHEN Tipo = 'Contado' THEN 1 ELSE NULL END ) as [Facturas de Contado],
+		COUNT( CASE WHEN Tipo = 'Credito' THEN 1 ELSE NULL END ) as [Facturas de Credito]
+FROM FacturaCab
+WHERE Estado = 'NOR'
+GROUP BY Pago
+ORDER BY Pago
+
+/*Clausula HAVING
+Permite filtrar los registros de una consulta luego de haber agrupado. HAVING se hace en base a
+los campos de agregado y se coloca despues del GROUP BY.*/
+
+/*Para las facturas cuyo estado sea NOR, muestre el total facturado por año y por mes
+ordenadas por Año y luego por Mes, incluya la cantidad de Facturas por cada grupo.
+Deje unicamente aquellos meses cuyo total facturado supere los 600000.*/
+SELECT	YEAR(Fecha) as Año, MONTH(Fecha) as Mes,
+		SUM(Total) as Total, COUNT(*) as Facturas
+FROM FacturaCab
+WHERE Estado = 'NOR'
+GROUP BY YEAR(Fecha), MONTH(Fecha)
+HAVING SUM(Total) > 600000
+ORDER BY 1,2
+
+/*Para las facturas cuyo estado sea NOR, muestre el total facturado por año y por mes
+ordenadas por Año y luego por Mes, incluya la cantidad de Facturas por cada grupo.
+Deje unicamente aquellos meses cuyo total facturado supere los 600000 y que tengan mas de 13000 facturas.*/
+SELECT	YEAR(Fecha) as Año, MONTH(Fecha) as Mes,
+		SUM(Total) as Total, COUNT(*) as Facturas
+FROM FacturaCab
+WHERE Estado = 'NOR'
+GROUP BY YEAR(Fecha), MONTH(Fecha)
+HAVING SUM(Total) > 600000 AND COUNT(*) > 13000
+ORDER BY 1,2
+
+/*Para la tabla de clientes (Customers) de la base de datos Northwind, muestre cuantos
+clientes hay por cada Pais (Country).
+Debe mostrar unicamente aquellos paises con 10 o mas clientes. Ordene la informacion por numero de clientes.*/
+SELECT Country, COUNT(*) as Clientes
+FROM Northwind.dbo.Customers
+GROUP BY Country
+HAVING COUNT(*) >= 10
+ORDER BY 2
+
+/*Para la tabla de clientes (Customers) de la base de datos Northwind, muestre cuantos
+clientes hay por cada Pais (Country).
+Debe mostrar unicamente aquellos paises que tengan entre 5 y 10 clientes. Ordene la informacion por numero de clientes.*/
+SELECT Country, COUNT(*) as Clientes
+FROM Northwind.dbo.Customers
+GROUP BY Country
+HAVING COUNT(*) BETWEEN 5 AND 10
+ORDER BY 2
+--puede usar todos los operadores que aprendio en WHERE dentro de HAVING; pero siempre
+--comando en cuenta que HAVING es exclusivo para funciones de agregado.
+
+--ejemplos de agrupamiento usando CASE WHEN -------------------------------------
+/*Case WHEN se puede formular de manera que responda de diversas formas de acuerdo a un valor de entrada
+que puede ser texto, nuero, calculo, etc.
+Ejemplo:
+Para la tabla Person de la base de datos AdventureWorks dentro del Rol Person
+realice la consulta siguiente:
+
+Muestre una columna llama Type
+la cual dependiendo del valor de PersonType mostrar un texto de acuerdo a la escala siguiente:
+EM	Empleado
+SP	Vendedor
+SC	Contacto de Tienda
+VC	Proveedor
+IN	Cliente
+cualquier otro valor que muestre el texto: Otros
+-> Muestre otra columna con la cantidad de clientes por cada Type de la columna anterior
+-> Deje unicamente aquellos grupos con 200 o mas personas
+Ordene los datos de acuerdo a esta ultima columna.
+*/
+
+SELECT	CASE PersonType
+			WHEN 'EM' THEN 'Empleado'
+			WHEN 'SP' THEN 'Vendedor'
+			WHEN 'SC' THEN 'Contacto de Tienda'
+			WHEN 'VC' THEN 'Proveedor'
+			WHEN 'IN' THEN 'Cliente'
+			ELSE 'Otros'
+		END as Type,
+		COUNT(*) as Cantidad
+FROM AdventureWorks.Person.Person
+GROUP BY CASE PersonType
+			WHEN 'EM' THEN 'Empleado'
+			WHEN 'SP' THEN 'Vendedor'
+			WHEN 'SC' THEN 'Contacto de Tienda'
+			WHEN 'VC' THEN 'Proveedor'
+			WHEN 'IN' THEN 'Cliente'
+			ELSE 'Otros'
+		END
+HAVING COUNT(*) > 200
+ORDER BY 2
+--Lastimosamente lo anterior obliga a escribir dos veces el campo calculado (CASE WHEN)
+--esto se puede evitar mediante el uso de subconsultas:
+
+SELECT Type, COUNT(*) as Cantidad
+FROM (
+	SELECT	CASE PersonType
+				WHEN 'EM' THEN 'Empleado'
+				WHEN 'SP' THEN 'Vendedor'
+				WHEN 'SC' THEN 'Contacto de Tienda'
+				WHEN 'VC' THEN 'Proveedor'
+				WHEN 'IN' THEN 'Cliente'
+				ELSE 'Otros'
+			END as Type
+	FROM AdventureWorks.Person.Person
+) as Subconsulta
+GROUP BY Type
+HAVING COUNT(*) > 200
+ORDER BY 2
+--Observe que la clasificacion de datos con CASE WHEN va dentro de la subconsulta.
+--fuera de la subconsulta hacemos el GROUP BY, HAVING y COUNT
